d19ed574cab00dc318983e1113ee6f0d3a451741
hami0033 <hannah.hamilton@ou.edu>
2021-09-16 16:23:34 -0500

    WPF Classes Participation

---
+++ Participations/WPFToys-Classes/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFToys_Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddToy_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string manufacturer, name, price, image;
+
+            manufacturer = txtManufacturer.Text;
+            name = txtName.Text;
+            price = txtPrice.Text;
+            image = txtImage.Text;
+
+            Toy newToy = new Toy(manufacturer, name, Convert.ToDouble(price), image);
+            var uri = new Uri(image);
+            var img = new BitmapImage(uri);
+
+
+            if (string.IsNullOrEmpty(manufacturer))
+            {
+                MessageBox.Show("Please enter a manufacturer.");
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Please enter a name.");
+
+                return;
+            }
+
+            if (double.TryParse(price))
+            {
+                MessageBox.Show("Please enter a valid price.");
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(image))
+            {
+                MessageBox.Show("Please enter an image URL.");
+
+                return;
+            }
+
+            lstToys.Items.Add(new Toy());
+
+        }
+
+        private void lstToys_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstToys.SelectedItem;
+            MessageBox.Show(selectedToy.GetAisle());
+        }
+    }
+}
---
+++ Participations/WPFToys-Classes/Toy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFToys_Classes
+{
+    public class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+        public Toy(string manufacturer, string name, double price, string image)
+        {
+            manufacturer = Manufacturer;
+            name = Name;
+            price = Price;
+            image = Image;
+        }
+
+        public string GetAisle()
+        {
+            string manufacturer = Manufacturer.ToUpper();
+
+            char[] manWord = manufacturer.ToCharArray();
+
+            char firstLetter = manWord[0];
+
+            Aisle = firstLetter.ToString();
+
+            return Aisle + Price;
+        }
+
+        public override string ToString()
+        {
+            string output = Manufacturer + " - " + Name;
+            return output;
+        }
+    }
+}
