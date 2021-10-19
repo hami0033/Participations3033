563692e891dbf8380c3852f9d78b649a15626b18
Hannah Hamilton <hannah.hamilton@ou.edu>
2021-10-19 16:15:01 -0500

    serialization

---
+++ Participations/JSONSerialization/Games.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONSerialization
+{
+    public class Games
+    {
+        public string name { get; set; }
+        public string platform { get; set; }
+        public DateTime releaseDate { get; set; }
+        public string summary { get; set; }
+        public int metaScore { get; set; }
+        public double userReview { get; set; }
+
+
+        public Games()
+        {
+            name = string.Empty;
+            platform = string.Empty;
+            releaseDate = DateTime.Now;
+            summary = string.Empty;
+            metaScore = 0;
+            userReview = 0;
+        }
+    }
+}
---
+++ Participations/JSONSerialization/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSONSerialization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Games> Game = new List<Games>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboPlatform.SelectedIndex = 0;
+            
+            var lines = File.ReadAllLines("all_games.csv").Skip(1);
+
+            foreach (var line in lines)
+            {
+                Game.Add(new Game(line));
+            }
+
+            PopulateComboBox(Game);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] pieces = lines[i].Split(',');
+
+                Games g = new Games(pieces[0], pieces[1], pieces[2], pieces[3], pieces[4], pieces[5]);
+                lstGames.Items.Add(g);
+            }
+
+            platforms = JsonConvert.SerializeObject<string[]>(json);
+            
+            cboPlatform.Items.Add("All");
+        }
+
+        private void PopulateComboBox(List<Games> game)
+        {
+            foreach (var platform in Games)
+            {
+                cboPlatform.Items.Add(platform);
+            }
+        }
+    }
+}
