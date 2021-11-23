0c3794a95adc13e8ca40976fb11629b997b131da
Hannah Hamilton <hannah.hamilton@ou.edu>
2021-11-23 17:46:46 -0600

    finished MVC

---
+++ Participations/MVCBeginner/Models/RandomChuckJokeAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBeginner.Models
+{
+    public class RandomChuckJokeAPI
+    {
+        public string icon_url { get; set; }
+
+        public string id { get; set; }
+
+        public string url { get; set; }
+
+        public string value { get; set; }
+    }
+}
